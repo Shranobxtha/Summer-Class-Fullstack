d2eca4f9133444b2316c7c70a65339b46194880b
Shranobxtha <gb11shranob@gmail.com>
2026-07-01 10:25:32 +0545

    tutorial2

---
+++ Tutorial 2/variable.js
@@ -0,0 +1,20 @@
+//let var const
+
+//the variable declared with var can be reassigned and redeclared
+// var a = 4; //declaration
+// a = 8; //value-reassign
+// var a = 9;
+// console.log(a);
+
+//the variable declared with let can be reassigned but cannot be replaced
+// let a = 3;
+// a = 5; //reassign
+// let a = 4; //redeclaration
+// console.log(a);
+
+//the variable declared with const cannot be reassigned and redeclared
+// const a = 2;
+// a = 1;
+// console.log(a);
+
+// let,const-> block scoped because they can only be accessed within the block where they are declared
